a70360996ee639846cbeeee7e4657fa9dc3e5898
Sudeep Reddy <sudeepreddy@Sudeeps-MacBook-Pro.local>
2020-07-20 19:08:56 -0500

    location permissions

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Home extends StatelessWidget {
+  void askLoc() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: Center(
-        child: Text(
-          "Restaurants",
-        ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            "Restaurants",
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 20,
+          ),
+          RaisedButton(
+            onPressed: askLoc,
+            child: Text('Allow Location'),
+          )
+        ],
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
   }
